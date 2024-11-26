7b76ec22cba7a22a2c43743c7f943b60d71d4b7e
hammefatal <hamsunhong@gmail.com>
2024-11-26 14:12:19 +0900

    first push

---
+++ app/src/main/java/gofdesignpatterns/creational/Singleton.java
@@ -0,0 +1,23 @@
+package gofdesignpatterns.creational;
+
+public class Singleton {
+    
+    // 정적 참조 변수 (싱글톤 객체를 담을 변수)
+    private static Singleton singletonObject;
+
+    // private 생성자
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (singletonObject == null) {
+            singletonObject = new Singleton();
+        }
+
+        return singletonObject;
+    }
+
+    public void doAction() {
+        System.out.println("Do Action by Singleton Object..");
+    }
+}
