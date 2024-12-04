0363f75fe62dccf72b4a0617c45f6e790181fa65
hammefatal <hamsunhong@gmail.com>
2024-12-04 23:46:53 +0900

    20241204#1

---
+++ app/src/main/java/gofdesignpatterns/creational/builder/directorbuilder/Data.java
@@ -0,0 +1,20 @@
+package gofdesignpatterns.creational.builder.directorbuilder;
+
+public class Data {
+
+    private String name;
+    private int age;
+
+    public Data(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
